8b45459d38f908d1abc8dd679bf73ca22afc6925
Your Name <you@example.com>
2025-12-13 19:01:23 +0900

    ポインタの再復習を開始

--- hello.c
+++ hello.c
@@ -1,45 +1,129 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 // ポインタについての追加学習
-typedef struct hello
-{
-    char h[56];
-    int num1;
-    int num2;
-}hello;
+// ポインタについてのさらなる深堀の学習
+int func_hoge(int *x, int *y){
+    *x = *x + *y;
+    *y = 56;
 
-int sum(int *x, int *y);
+}
+int get_word(char buf[], int buf_size, FILE *fp){
+    int len;
+    int ch;
+
+    while ((ch = getc(fp) != EOF && !isalnum(ch)));
+    if (ch == EOF)
+    {
+        return EOF;
+    }
+    len = 0;
+    do
+    {
+        buf[len] = ch;
+        len++;
+        if (len >= buf_size)
+        {
+            fprintf(stderr, "word to long. \n");
+            exit(1); // 異常終了
+        }
+        
+    } while ((ch = getc(fp) != EOF && !isalnum(ch)));
+    
+    buf[len] = '\0';
+    return len;
+    
+}
 int main(void)
 {
-    int apple = 100;
-    int orange = 150;
-    int *b = &apple;
-    int *c;
-    c = &apple;
+    /*基本の復習*/
+    int hoge = 5;
+    int piyo = 10;
+    int *hoge_p; // int型から派生したポインタ
 
-    printf("%d\n", *b + *c);
-    printf("%d\n", apple + *c);
-    printf("%p\n", &apple);
-    printf("%p\n", b);
+    // アドレスの確認
+    printf("%p\n", &hoge);
+    printf("%p\n", &piyo);
+    printf("%p\n", &hoge_p);
 
-    sum(&apple, &orange);
-    printf("%d, %d\n", apple, orange);
+    hoge_p = &hoge;
+    printf("変更後のhoge_p: %p\n", &hoge_p);
 
-    hello hello1 = {"hello", 10, 56};
-    hello *hello2 = &hello1;
-    printf("%d\n", hello2->num1);
+    printf("%d\n", *hoge_p);
+    *hoge_p = 10; // ポインタの指す先の値を変更
+    printf("%d\n", *hoge_p);
+    printf("%p\n", hoge_p);
 
-    // ポインタの大きさについて
-    printf("%zu\n",sizeof(int*));
-    printf("%zu\n",sizeof(char*));
-    printf("%zu\n",sizeof(void*));
+    /*演算子*/
+    // アドレス演算子 = &
+    // 関節演算子　=　*
+    // 添字演算子　= []
+
+    /*ポインタ演算 */
+   // hoge_p++;
+    printf("%p\n", hoge_p + 3);
+
+    // 複数の値をポインタを使い返す
+    int x = 5;
+    int y = 6;
+    func_hoge(&x, &y);
+
+    printf("%d\n", x);
+    printf("%d\n", y);
+
+    /*配列*/
+    int array[5];
+    int i;
+    int *p;
+    for (i = 0; i < 5; i++)
+    {
+        array[i] = i;
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    for (p = &array[0]; p != &array[5]; p++)
+    {
+        printf("%d ", *p);
+    }
+    printf("\n");
+    
+    /*char buf[256];
+    while (get_word(buf, 256, stdin) != EOF)
+    {
+        printf("<<%s>>\n", buf);
+    }*/
+    
+    /*可変長配列 */
+    int size1, size2, size3;
+    scanf("%d %d %d", &size1, &size2, &size3);
+
+    int array1[size1];
+    int array2[size2][size3];
+    for (int i = 0; i < size1; i++)
+    {
+        array1[i] = i;
+    }
+    for (int i = 0; i < size2; i++)
+    {
+        for (int j = 0; j < size3; j++)
+        {
+            array2[i][j] = i * size3 * j;
+        }
+        
+    }
+    for (int i = 0; i < size1; i++)
+    {
+        printf("[%d]: %d\n", i, array1[i]);
+    }
+    for (int i = 0; i < size2; i++)
+    {
+        for (int j = 0; j < size3; j++)
+        {
+            printf("\t%d", array2[i][j]);
+        }
+        printf("\n");
+    }
+    
     return 0;
-}
-
-// この関数からmain関数の値を操作
-int sum(int *x, int *y){
-    *x = 25 + *y;
-    *y = 50 + 5;
-
 }
